2c66a6ce2c627ede03c03ace558571964516dbc6
harishrao7 <ailneniharishrao7@gmail.com>
2023-08-10 10:30:25 -0400

    model

---
+++ src/main/java/com/example/employee/model/Address.java
@@ -0,0 +1,13 @@
+package com.example.employee.model;
+
+public class Address {
+
+	private String hNO;
+	private String addLine1;
+	private String addLine2;
+	private String city;
+	private String state;
+	private String country;
+	private String zipCode;
+	
+}
